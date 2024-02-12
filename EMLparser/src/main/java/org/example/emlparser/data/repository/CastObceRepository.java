77ad33440ca1739e8ddd59595e1c879f90fff298
Artem Otliaguzov <temaotl0992@gmail.com>
2024-02-12 14:38:52 +0100

    fix naming

--- EMLparser/src/main/java/org/example/emlparser/data/repository/CastObceRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.emlparser.data.repository;
-
-import org.example.emlparser.data.entity.CastObce;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface CastObceRepository extends CrudRepository<CastObce,String> {
-}
--- EMLparser/src/main/java/org/example/emlparser/data/repository/ObecRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.emlparser.data.repository;
-
-import org.example.emlparser.data.entity.Obec;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ObecRepository extends CrudRepository<Obec,String> {
-}
